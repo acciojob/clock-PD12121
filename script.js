52cb669d447a5bf84c9308f3a763151554a6eee6
PD12121 <115214172+PD12121@users.noreply.github.com>
2023-07-04 19:57:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+function updateclock() {
+	const currTime = new Date();
+	const hours = currTime.getHours();
+	const minutes = currTime.getMinutes();
+	const seconds = currTime.getSeconds();
+
+	
+}
