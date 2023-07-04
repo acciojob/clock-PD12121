407f2efdce58db6b448d77214246ad5c563c776b
PD12121 <115214172+PD12121@users.noreply.github.com>
2023-07-04 20:00:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,6 +7,18 @@ function updateclock() {
 	const seconds = currTime.getSeconds();
 
 
+	 minutes = (minutes < 10 ? "0" : "") + minutes;
+        seconds = (seconds < 10 ? "0" : "") + seconds;
+
+        // Determine AM/PM
+        var period = hours < 12 ? "AM" : "PM";
+
+        // Convert to 12-hour format
+        hours = hours > 12 ? hours - 12 : hours;
+        hours = hours === 0 ? 12 : hours;
+
+	
 	var clockDiv = document.getElementById("clock");
 	clockDiv.textContent = hours + ":" + minutes + ":" + seconds + " " + period;
 }
+setInterval(updateClock, 1000);
